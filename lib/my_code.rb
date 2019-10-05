920e51a84d4aa8f3584fd1a3f39979080a21dd2f
Nathan Alex <nate745@gmail.com>
2019-10-05 20:35:33 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -49,7 +49,7 @@ def reduce_to_total(source_array,starting_point = 0)
 end
 
 def reduce_to_all_true(source_array)
-  i = o
+  i = 0
   while i < source_array.length do
     return false if !source_array[i]
     i +=1
